2b65a1db14a36c6df69d60a2e57072465946fd02
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-11-09 06:52:40 +0700

    feat: add login and jwt

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -4,23 +4,25 @@ import (
 	"fmt"
 	"net/http"
 	"project4/helper"
+	"project4/middleware"
 	"project4/model/input"
 	"project4/model/response"
 	"project4/service"
 
 	"github.com/asaskevich/govalidator"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
-type UserController struct {
+type userController struct {
 	userService service.UserService
 }
 
-func NewUserController(userService service.UserService) *UserController {
-	return &UserController{userService}
+func NewUserController(userService service.UserService) *userController {
+	return &userController{userService}
 }
 
-func (h *UserController) RegisterUser(c *gin.Context) {
+func (h *userController) RegisterUser(c *gin.Context) {
 	var input input.UserRegisterInput
 
 	err := c.ShouldBindJSON(&input)
@@ -57,7 +59,70 @@ func (h *UserController) RegisterUser(c *gin.Context) {
 	c.JSON(201, response)
 }
 
-func (h *UserController) UpdateUser(c *gin.Context) {
+func (h *userController) LoginUser(c *gin.Context) {
+	var input input.UserLoginInput
+
+	err := c.ShouldBindJSON(&input)
+
+	login, err := govalidator.ValidateStruct(input)
+
+	if !login {
+		response := helper.APIResponse("failed", gin.H{
+			"errors": err.Error(),
+		})
+
+		c.JSON(http.StatusBadRequest, response)
+		fmt.Println("error: " + err.Error())
+		return
+	}
+
+	// send to services
+	// get user by email
+	user, err := h.userService.GetUserByEmail(input.Email)
+
+	if err != nil {
+		response := helper.APIResponse("failed", gin.H{
+			"errors": err.Error(),
+		})
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	// return when user not found!
+	if user.ID == 0 {
+		errorMessages := "User not found!"
+		response := helper.APIResponse("failed", errorMessages)
+		c.JSON(http.StatusNotFound, response)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password))
+
+	if err != nil {
+		response := helper.APIResponse("failed", "password not match!")
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// create token
+	jwtService := middleware.NewService()
+	token, err := jwtService.GenerateToken(user.ID)
+	if err != nil {
+		response := helper.APIResponse("failed", "failed to generate token!")
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	loginResponse := response.UserLoginResponse{
+		Token: token,
+	}
+
+	// return token
+	response := helper.APIResponse("ok", loginResponse)
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *userController) UpdateUser(c *gin.Context) {
 	var inputUserUpdate input.UserUpdateInput
 
 	err := c.ShouldBindJSON(&inputUserUpdate)
@@ -120,12 +185,12 @@ func (h *UserController) UpdateUser(c *gin.Context) {
 		Balance:   userUpdated.Balance,
 		UpdatedAt: userUpdated.UpdatedAt,
 	}
-	
+
 	response := helper.APIResponse("ok", updatedResponse)
 	c.JSON(http.StatusOK, response)
 }
 
-func (h *UserController) DeleteUser(c *gin.Context) {
+func (h *userController) DeleteUser(c *gin.Context) {
 	var idUserUri input.UserDeleteID
 
 	err := c.ShouldBindUri(&idUserUri)
@@ -157,7 +222,7 @@ func (h *UserController) DeleteUser(c *gin.Context) {
 	deletedResponse := response.UserDeleteResponse{
 		Message: "User has been deleted",
 	}
-	
+
 	response := helper.APIResponse("ok", deletedResponse)
 	c.JSON(http.StatusOK, response)
 }
